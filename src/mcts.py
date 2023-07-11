2309cef54d47f0bef47ed28c666bc1f0e7f24460
Emmanuel Larralde <l4rralde@Emmanuels-MacBook-Air.local>
2023-07-10 19:50:54 -0600

    Monte Carlo Tree Search

---
+++ src/mcts.py
@@ -0,0 +1,122 @@
+import random
+import copy
+import math
+
+class Node:
+	def _init_(self, game, done, parent, observation, action_idx) -> None:
+		self.child = None #Child nodes
+		self.T = 0 #Represents the sum of the value of the rollouts that have been started from this node
+		self.N = 0 #Visit count
+		self.game = game #The environment
+		self.observation = observation #Current state of the game in the node
+		self.done = done #Game is done
+		self.parent = parent #Parent node. For backpropagation
+		self.action_idx = action_idx #Index of the action that lead to this node
+
+		self.c = 1.0 #FOR UCB score???
+
+	def getUCBscore(self) -> float:
+		"""
+		Upper Confidence Bound
+		"""
+		if self.N == 0:
+			return float('inf')
+
+		if self.parent:
+			top_node = self.parent
+		else:
+			top_node = self
+
+		return (self.T/self.N)+self.c*math.sqrt((math.log(top_node.N)/self.N))
+
+	def create_child(self) -> None:
+		if self.done:
+			return
+
+		child = {}
+		for action in range(self.game.action_space.n):
+			game = copy.deepcopy(self.game)
+			observation, reward, done, truncated, info = game.step(action)
+			child[action] = Node(game, done, self, observation, action)
+
+		self.child = child
+
+	def explore(self) -> None:
+		"""
+		The search algorithm is as follows:
+		- From the current node, recursively pick the children which maximizes the value
+		- When a leaf is reached:
+			- If it has never been explored before, do a rollout and update its current value
+			- Otherwise, expand the node creating its children, randomly pick one, do a rollout and update its value
+		- Backpropagate
+		"""
+		current = self
+		while current.child:
+			child = current.child
+			max_U = max(children.getUCBscore() for children in child.values())
+			actions = [a for a,c in child.items() if c.getUCBscore()==max_U]
+			if len(actions) == 0:
+				raise ValueError(f"Error zero length: {max_U}")
+			action = random.choice(actions)
+			current = child[action]
+
+		#Play a random game, or expand if needed
+		if current.N < 1:
+			current.T += current.rollout()
+		else:
+			current.create_child()
+			if current.child:
+				current = random.choice(current.child)
+			current.T += current.rollout()
+
+		#Update statistics and backpropagate
+		current.N += 1
+		parent = current
+		while parent.parent:
+			parent = parent.parent
+			parent.N += 1
+			parent.T += current.T
+
+	def rollout(self) -> int:
+		if self.done:
+			return 0
+
+		total_reward = 0
+		new_game = copy.deepcopy(self.game)		
+		done = False
+		while not done:
+			action = new_game.action_space.sample()
+			observation, reward, done, truncated, info = new_game.step(action)
+			total_reward += reward
+			if done:
+				new_game.reset()
+				new_game.close()
+				break
+
+		return total_reward
+
+	def next(self):
+		"""
+		Ask for the next action to play from the current node
+		"""
+		if self.done:
+			raise ValueError("Game over")
+
+		if not self.child:
+			raise ValueError("No children found and game is not over")
+
+		child = self.child
+		max_N = max(node.N for node in child.values())
+		max_children = [c for a,c in child.items() if c.N == max_N]
+
+		if len(max_children) == 0:
+			raise ValueError(f"Error zero length: {max_children}")
+
+		max_child = random.choice(max_children)
+
+		return max_child
+
+	def detach_parent(self) -> None:
+		"""free memory detaching nodes"""
+		del self.parent
+		self.parent = None
---
+++ src/mcts_cartpole.py
@@ -0,0 +1,59 @@
+import copy
+from time import sleep
+
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+
+import mcts
+
+
+def policy_player_mcts(mytree, policy_explore: int=100) -> tuple:
+	for _ in range(policy_explore):
+		mytree.explore()
+
+	next_node = mytree.next()
+	next_node.detach_parent()
+	return next_node
+
+	
+def main() -> None:
+	GAME_NAME = "CartPole-v1"
+	MAX_ATTEMPTS = 100
+
+	episodes = 10
+	rewards = []
+	moving_average = []
+
+	for episode in range(episodes):
+		print(f"episode #{episode+1}")
+
+		reward_e = 0
+		game = gym.make(GAME_NAME, render_mode="rgb_array")
+		observation, info = game.reset() #Initial state
+		new_game = copy.deepcopy(game)
+		mytree = mcts.Node(new_game, False, None, observation, 0) #MCTS
+
+		done = False
+		attempt_cnt = 0
+		while not done and attempt_cnt<MAX_ATTEMPTS:
+			mytree = policy_player_mcts(mytree)
+			observation, reward, done, truncated, info = game.step(mytree.action_idx)
+			print(f"episode #{episode}, observation={observation}, reward={reward}")
+			reward_e += reward
+			attempt_cnt += 1
+			game.render()
+			sleep(1)
+
+		print(f"reward_e: {reward_e}")
+		game.close()
+		rewards.append(reward_e)
+		moving_average.append(np.mean(rewards[-100:]))
+	plt.plot(rewards)
+	plt.plot(moving_average)
+	plt.show()
+	print(f"moving_average: {np.mean(rewards[-20:])}")
+
+
+if _name_ == '_main_':
+	main()
