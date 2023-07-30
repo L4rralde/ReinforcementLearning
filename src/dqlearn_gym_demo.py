ad2a58c59801a25cb351f58322313dcaddc7e806
L4rralde <ealarralde@gmail.com>
2023-07-30 16:36:13 -0600

    Using stable_baselines wrappers

--- src/dqlearn_gym_demo.py
+++ src/dqlearn_gym_demo.py
@@ -1,17 +1,36 @@
 import random
 
 import gym
+from stable_baselines3.common.atari_wrappers import (
+    ClipRewardEnv,
+    EpisodicLifeEnv,
+    FireResetEnv,
+    MaxAndSkipEnv,
+    NoopResetEnv,
+)
+
 from dqlearn import AtariDeepQLearning
 
 
-def main():
-    env = gym.make("BreakoutNoFrameskip-v4", render_mode="human")
+def make_env():
+    env = gym.make("Boxing-v4", render_mode="human")
+    env = gym.wrappers.RecordEpisodeStatistics(env)
+    env = NoopResetEnv(env, noop_max=30)
+    env = MaxAndSkipEnv(env, skip=4)
+    env = EpisodicLifeEnv(env)
+    if "FIRE" in env.unwrapped.get_action_meanings():
+        env = FireResetEnv(env)
+    env = ClipRewardEnv(env)
     env = gym.wrappers.ResizeObservation(env, (84, 84))
     env = gym.wrappers.GrayScaleObservation(env)
     env = gym.wrappers.FrameStack(env, 4)
+    return env
+
+def main():
+    env = make_env()
     deep_q_agent = AtariDeepQLearning(env)
-    deep_q_agent.load('dqln.model')
-    mean_reward, std_reward= deep_q_agent.evaluate(n_eval_episodes=100, seeds=[random.randint(0,1000) for _ in range(10)])
+    deep_q_agent.load('dqln_boxing.model')
+    mean_reward, std_reward= deep_q_agent.evaluate(n_eval_episodes=10, seeds=[random.randint(0,1000) for _ in range(10)])
     print(f"Mean reward: {mean_reward}, standard deviation of reward: {std_reward}")
 
 
--- src/dqlearn_gym_train.py
+++ src/dqlearn_gym_train.py
@@ -1,16 +1,41 @@
 import gym
+from stable_baselines3.common.atari_wrappers import (
+    ClipRewardEnv,
+    EpisodicLifeEnv,
+    FireResetEnv,
+    MaxAndSkipEnv,
+    NoopResetEnv,
+)
+import matplotlib.pyplot as plt
 
 from dqlearn import AtariDeepQLearning
 
 
-def main():
-    env = gym.make("BreakoutNoFrameskip-v4")
+def make_env():
+    env = gym.make("Boxing-v4")
+    env = gym.wrappers.RecordEpisodeStatistics(env)
+    env = NoopResetEnv(env, noop_max=30)
+    env = MaxAndSkipEnv(env, skip=4)
+    env = EpisodicLifeEnv(env)
+    if "FIRE" in env.unwrapped.get_action_meanings():
+        env = FireResetEnv(env)
+    env = ClipRewardEnv(env)
     env = gym.wrappers.ResizeObservation(env, (84, 84))
     env = gym.wrappers.GrayScaleObservation(env)
     env = gym.wrappers.FrameStack(env, 4)
+    return env
+
+def main():
+    env = make_env()
     deep_q_agent = AtariDeepQLearning(env)
-    deep_q_agent.train()
-    deep_q_agent.save('dqln.model')
+    rewards = deep_q_agent.train(total_steps=1e6,
+                                         learning_start=2e5,
+                                         buffer_size=int(1e4),
+                                         exploration_fraction=0.3,
+                                         gamma=0.95)
+    deep_q_agent.save('dqln_boxing.model')
+    plt.plot(rewards)
+    plt.show()
 
 
 if __name__ == '__main__':
